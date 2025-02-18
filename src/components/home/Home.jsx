f5bbbb62c7687f900846ea7ddcc3ed7d4ee9d8f9
Waleed <wsaleh1991@gmail.com>
2025-02-17 19:34:26 -0600

    2 form inputs added need to work on placement and styling

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,25 @@
-import FormContainer from "../form/FormContainer";
+import { FloatingLabel, Form } from "react-bootstrap";
+
+import "./Home.css";
 
 function Home() {
   return (
-    <div>
-      
+    <div className="home-container">
+      <div className="form-container">
+        <FloatingLabel controlId="floatingInput" className="mb-3 ">
+          <Form.Control
+            type="text"
+            placeholder="First Name"
+            className="form-input"
+          />
+        </FloatingLabel>
+        <Form.Select aria-label="Default select example" className="form-input">
+          <option>Select Category</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+      </div>
     </div>
   );
 }
