7c0dc7380cbe30098704d4a3e102e0635c493816
Waleed <wsaleh1991@gmail.com>
2025-03-04 18:38:34 -0600

    questions are appearing now. Styling is required and additional parts of the question cards required to be added

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
-import { data, useLocation } from "react-router-dom";
-import useFetch from "../../hooks/useFetch";
+import { Card, Form } from "react-bootstrap";
+
+import "./Quiz.css";
 
 function Quiz() {
   const location = useLocation();
@@ -9,10 +12,38 @@ function Quiz() {
 
   const categoryId = quizData.category.split(" ")[0];
   const [quizQuestions, setQuizQuestions] = useState([]);
-  
+
+  useEffect(() => {
+    async function getQuestions() {
+      try {
+        const response = await axios.get(
+          `https://opentdb.com/api.php?amount=10&category=${categoryId}&difficulty=${quizData.difficulty}&type=multiple`,
+          { timeout: 5000 }
+        );
+        setQuizQuestions(response.data.results);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getQuestions();
+  }, [categoryId, quizData.difficulty]);
+
   return (
-    <div>
-      <ul></ul>
+    <div className="quiz-container">
+      <div className="questions-container">
+        {quizQuestions.map((ques, index) => (
+          <Card
+            style={{ width: "18rem" }}
+            className="question-card"
+            key={index}
+          >
+            <Form>
+              <Card.Header>Question #{index}</Card.Header>
+              <Card.Text>{ques.question}</Card.Text>
+            </Form>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
