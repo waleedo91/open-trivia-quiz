17b875068af52246a5be8d811f7ee98cb9e7073b
Waleed <wsaleh1991@gmail.com>
2025-03-08 02:37:08 -0600

    Nav bar finished and updated to look a lot better and updated home button as well.

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 
-import { Card, Form } from "react-bootstrap";
+import { Button, Card, Form } from "react-bootstrap";
 
 import "./Quiz.css";
 
@@ -12,6 +12,7 @@ function Quiz() {
 
   const categoryId = quizData.category.split(" ")[0];
   const [quizQuestions, setQuizQuestions] = useState([]);
+  const [answers, setAnswers] = useState([]);
 
   useEffect(() => {
     async function getQuestions() {
@@ -28,28 +29,81 @@ function Quiz() {
     getQuestions();
   }, [categoryId, quizData.difficulty]);
 
+  const updateAnswers = () => {
+    quizQuestions.incorrect_answers.map((answer) => setAnswers(answer));
+    console.log(answers);
+  };
+
+  updateAnswers();
+
   return (
-    <div className="quiz-container">
-      <div className="questions-container">
-        {quizQuestions.map((ques, index) => (
-          <Card className="question-card" key={index}>
-            <Form>
-              <Card.Header>Question #{index + 1}</Card.Header>
-              <Card.Text>{ques.question}</Card.Text>
-              {ques.incorrect_answers.map((answers, index) => (
-                <Form.Check key={index} type="radio" label={answers} />
-              ))}
-              <Form.Check
-                type="radio"
-                id={ques.correct_answer}
-                label={ques.correct_answer}
-              />
-            </Form>
-          </Card>
-        ))}
-      </div>
-    </div>
+    <Form>
+      {/* {quizQuestions.map((quizQuestions, index) => (
+        console.log(qui)
+      ))} */}
+    </Form>
   );
 }
 
 export default Quiz;
+
+/* <div className="quiz-container">
+      <div className="questions-container">
+        <Form>
+          {quizQuestions.map((ques, index) => (
+            <Card className="question-card" key={index}>
+              <Card.Header>Question #{index + 1}</Card.Header>
+              <Card.Text>{ques.question}</Card.Text>
+              {ques.incorrect_answers.map((answers, index) => (
+                <Form.Check
+                  key={index}
+                  name={`answer ${index}`}
+                  type="radio"
+                  label={answers}
+                  // onChange={handleChange}
+                />
+              ))}
+              <Form.Check
+                type="radio"
+                name={`answer ${index}`}
+                id={ques.correct_answer}
+                label={ques.correct_answer}
+                // onChange={handleChange}
+              />
+            </Card>
+          ))}
+          <Button className="quiz-button" type="submit">
+            Submit!
+          </Button>
+        </Form>
+      </div>
+    </div> */
+
+
+
+
+
+
+
+
+
+
+    <>
+      <Form onSubmit={handleSubmit}>
+        {questions.map((question, i) => (
+          <Form.Group key={i}>
+            <h6>Question #{i + 1}</h6>
+            <p>{question.question}</p>
+            {question.answers.map((answer, index) => (
+              <Form.Check
+                key={index}
+                type="radio"
+                name={answer}
+                label={answer}
+              />
+            ))}
+          </Form.Group>
+        ))}
+        <Button type="submit">Submit!</Button>
+      </Form>
+    </>
